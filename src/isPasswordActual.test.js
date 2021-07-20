8fbdfc6576c1fb79b3c971a605817678b9405ed0
TserekhArthur <slash33slash3@gmail.com>
2021-07-20 17:30:44 +0300

    isPasswordActual solution

--- src/isPasswordActual.test.js
+++ src/isPasswordActual.test.js
@@ -4,17 +4,55 @@ describe(`Function 'isPasswordActual':`, () => {
   const isPasswordActual = require('./isPasswordActual');
 
   it(`should be declared`, () => {
-    expect(isPasswordActual).toBeInstanceOf(Function);
+    expect(isPasswordActual)
+      .toBeInstanceOf(Function);
   });
 
   it(`should return a string`, () => {
-
+    expect(typeof isPasswordActual(2020, 7, 20))
+      .toBe('string');
   });
 
-  it(`should ask to change the password if was changed a year ago`, () => {
-    expect(isPasswordActual(2020, 6, 9))
+  it(`should ask to change the password if it was changed a year ago`, () => {
+    expect(isPasswordActual(2020, 7, 20))
       .toBe('Immediately change the password!');
   });
 
-  // write more tests here
+  it(`should ask to change the password if it was changed 61 days ago`, () => {
+    expect(isPasswordActual(2021, 5, 20))
+      .toBe('Immediately change the password!');
+  });
+
+  it(`should ask to change the password if it was changed 60 day ago`, () => {
+    expect(isPasswordActual(2021, 5, 21))
+      .toBe('You should change your password.');
+  });
+
+  it(`should ask to change the password if it was changed 35 days ago`, () => {
+    expect(isPasswordActual(2021, 6, 15))
+      .toBe('You should change your password.');
+  });
+
+  it(`should ask to change the password if it was changed 31 day ago`, () => {
+    expect(isPasswordActual(2021, 6, 19))
+      .toBe('You should change your password.');
+  });
+
+  it(`should message that password is
+    actual if it was changed 30 days ago`, () => {
+    expect(isPasswordActual(2021, 6, 20))
+      .toBe('Password is actual.');
+  });
+
+  it(`should message that password is
+    actual if it was changed 1 day ago`, () => {
+    expect(isPasswordActual(2021, 7, 19))
+      .toBe('Password is actual.');
+  });
+
+  it(`should message that password is
+    actual if it was changed today`, () => {
+    expect(isPasswordActual(2021, 7, 20))
+      .toBe('Password is actual.');
+  });
 });
