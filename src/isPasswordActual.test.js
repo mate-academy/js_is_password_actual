2bab3688e0a9092fd9230e87cff6e8c840f697ad
varopn <varopn@gmail.com>
2021-07-20 16:53:47 +0300

    Solution

--- src/isPasswordActual.test.js
+++ src/isPasswordActual.test.js
@@ -8,13 +8,36 @@ describe(`Function 'isPasswordActual':`, () => {
   });
 
   it(`should return a string`, () => {
-
+    expect(typeof isPasswordActual(2020, 9, 6)).toBe('string');
   });
 
-  it(`should ask to change the password if was changed a year ago`, () => {
+  it(`should ask 'Immediately change the password!'
+      if was changed a year ago`, () => {
     expect(isPasswordActual(2020, 6, 9))
       .toBe('Immediately change the password!');
   });
 
-  // write more tests here
+  it(`should ask 'Password is actual'
+      if was changed a 5 days ago`, () => {
+    expect(isPasswordActual(2021, 7, 15))
+      .toBe('Password is actual.');
+  });
+
+  it(`should ask 'You should change your password.'
+    if was changed a 35 days ago`, () => {
+    expect(isPasswordActual(2021, 6, 15))
+      .toBe('You should change your password.');
+  });
+
+  it(`should ask 'Password is actual'
+      if was changed a 30 days ago`, () => {
+    expect(isPasswordActual(2021, 6, 20))
+      .toBe('Password is actual.');
+  });
+
+  it(`should ask 'Immediately change the password!'
+    if was changed 60 days ago`, () => {
+    expect(isPasswordActual(2020, 5, 21))
+      .toBe('Immediately change the password!');
+  });
 });
