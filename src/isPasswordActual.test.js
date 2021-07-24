530fb437c62fa5fa8ef7e8ae653dc43b6c3881ce
Dana Oliynyk <dana.victorivna23@gmail.com>
2021-07-24 11:54:20 +0300

    more tests

--- src/isPasswordActual.test.js
+++ src/isPasswordActual.test.js
@@ -16,9 +16,39 @@ describe(`Function 'isPasswordActual':`, () => {
       .toBe('Immediately change the password!');
   });
 
+  it(`should return message that password should changed 
+  if password uptaded 61 days ago`, () => {
+    expect(isPasswordActual(2021, 5, 22))
+      .toBe('Immediately change the password!');
+  });
+
+  it(`should return message that password should changed 
+  if password uptaded 60 days ago`, () => {
+    expect(isPasswordActual(2021, 5, 25))
+      .toBe('You should change your password.');
+  });
+
   it(`should return message that password is actual if changed today`, () => {
-    expect(isPasswordActual(2021, 7, 22))
+    expect(isPasswordActual(2021, 7, 24))
       .toBe('Password is actual.');
+  });
+
+  it(`should return message that password is actual
+   if changed 1 day ago`, () => {
+    expect(isPasswordActual(2021, 7, 23))
+      .toBe('Password is actual.');
+  });
+
+  it(`should return message that password is actual
+   if changed 30 day ago`, () => {
+    expect(isPasswordActual(2021, 6, 24))
+      .toBe('Password is actual.');
+  });
+
+  it(`should return message that password should changed 
+  if password uptaded 31 days ago`, () => {
+    expect(isPasswordActual(2021, 6, 23))
+      .toBe('You should change your password.');
   });
 
   it(`should return message that password should changed 
