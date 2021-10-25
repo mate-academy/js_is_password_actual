e983422d17a50ec63e8742346469d9ae677981fd
karinababii <karino4ka201351@gmail.com>
2021-10-25 18:23:51 +0300

    Solution2

--- src/isPasswordActual.test.js
+++ src/isPasswordActual.test.js
@@ -11,7 +11,7 @@ describe(`Function 'isPasswordActual':`, () => {
     expect(typeof isPasswordActual(2020, 6, 9)).toBe('string');
   });
 
-  it(`should ask to change the password if was changed a year ago`, () => {
+  it(`should ask to change the password if it was changed a year ago`, () => {
     expect(isPasswordActual(2020, 6, 9))
       .toBe('Immediately change the password!');
   });
