3a5d7e611883bf7451d0d0f7ddb3d18c07a286a6
NikoLazur <lazur.niko@gmail.com>
2021-10-26 15:00:51 +0300

    Added tests

--- src/isPasswordActual.test.js
+++ src/isPasswordActual.test.js
@@ -12,7 +12,7 @@ describe(`Function 'isPasswordActual':`, () => {
   });
 
   it(`should ask to change the password if was changed a year ago`, () => {
-    expect(isPasswordActual(2020, 6, 9))
+    expect(isPasswordActual(2020, 10, 25))
       .toBe('Immediately change the password!');
   });
 
@@ -23,9 +23,27 @@ describe(`Function 'isPasswordActual':`, () => {
   });
 
   it(`should ask to change the password if 
-    was changed more than 2 months ago`, () => {
+    was changed 60 days ago`, () => {
+    expect(isPasswordActual(2021, 8, 26))
+      .toBe('Immediately change the password!');
+  });
+
+  it(`should ask to change the password if 
+    was changed 61 day ago`, () => {
     expect(isPasswordActual(2021, 8, 25))
       .toBe('Immediately change the password!');
+  });
+
+  it(`should ask to change the password if 
+    was changed 90 days ago`, () => {
+    expect(isPasswordActual(2021, 7, 25))
+      .toBe('Immediately change the password!');
+  });
+
+  it(`should shows a message that password is actual
+     if was changed 0 days ago`, () => {
+    expect(isPasswordActual(2021, 10, 26))
+      .toBe('Password is actual.');
   });
 
   it(`should shows a message that password is actual
