5bc2157998d02fe9e11caca45a85a6f1595f6117
Vlad vlad <you@example.com>
2022-01-06 22:01:55 +0200

    4thtaskisready

--- src/isPasswordActual.test.js
+++ src/isPasswordActual.test.js
@@ -7,14 +7,38 @@ describe(`Function 'isPasswordActual':`, () => {
     expect(isPasswordActual).toBeInstanceOf(Function);
   });
 
-  it(`should return a string`, () => {
-
+  it(`shouldn't ask to change the password if password was changed today`, () => {
+    expect(isPasswordActual(2022, 1, 6)) 
+      .toBe('Password is actual.')
   });
 
-  it(`should ask to change the password if was changed a year ago`, () => {
-    expect(isPasswordActual(2020, 6, 9))
+  it(`shouldn't ask to change the password if password was changed two weeks ago`, () => {
+    expect(isPasswordActual(2021,12,22)) .toBe('Password is actual.');
+  });
+
+  it(`shouldn't ask to change the password if password was changed 30 days ago`, () => {
+    expect(isPasswordActual(2021,12, 7)) .toBe('Password is actual.');
+  });
+
+  it(`should recommend to change the password if password was changed 31 days ago`, () => {
+    expect(isPasswordActual(2021,12, 6)) .toBe('You should change your password.');
+  });
+
+  it(`shouldn't ask to change the password if password was changed 45 days ago`, () => {
+    expect(isPasswordActual(2021,11, 21)) .toBe('You should change your password.');
+  });
+
+  it(`shouldn't ask to change the password if password was changed 60 days ago`, () => {
+    expect(isPasswordActual(2021,11, 7)) .toBe('You should change your password.');
+  });
+
+  it(`should ask to change the password if was changed 61 days ago`, () => {
+    expect(isPasswordActual(2021,11, 6))
       .toBe('Immediately change the password!');
   });
 
-  // write more tests here
+  it(`should ask to change the password if was changed a year ago`, () => {
+    expect(isPasswordActual(2021, 1, 6))
+      .toBe('Immediately change the password!');
+  });
 });
