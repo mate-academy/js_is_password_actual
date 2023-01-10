e060f794c1bc0ac9c82a0420f1e592c6f0d44b57
Usenko Bohdana <usenkobogdana19@gmail.com>
2023-01-10 22:50:13 +0200

    solution

--- src/isPasswordActual.test.js
+++ src/isPasswordActual.test.js
@@ -2,27 +2,29 @@
 
 describe(`Function 'isPasswordActual':`, () => {
   const isPasswordActual = require('./isPasswordActual');
-  const date = new Date(Date.now());
-  const today = {
-    year: date.getUTCFullYear(),
-    month: date.getMonth() + 1,
-    date: date.getDate(),
-  };
 
   it(`should be declared`, () => {
     expect(isPasswordActual).toBeInstanceOf(Function);
   });
 
   it(`should return a string`, () => {
-
+    expect(typeof isPasswordActual(2022, 1, 9)).toBe('string');
   });
 
-  it(`should ask to change the password if was changed a year ago`, () => {
-    const lastYear = isPasswordActual(today.year - 1, today.month, today.date);
-
-    expect(lastYear)
+  it(`should ask to change the password if it was changed a year ago`, () => {
+    expect(isPasswordActual(2020, 6, 9))
       .toBe('Immediately change the password!');
   });
 
-  // write more tests here
+  it(`should return password is actual if
+    it was changed less than 30 days ago`, () => {
+    expect(isPasswordActual(2023, 10, 1))
+      .toBe('Password is actual.');
+  });
+
+  it(`should ask to change the password if it  was changed
+    more than mounth ago`, () => {
+    expect(isPasswordActual(2022, 12, 10))
+      .toBe('You should change your password.');
+  });
 });
