c617a08de4e13ee8c11f618bcb1449603588e503
oleksandrshtonda <oleksandrshtonda@gmail.com>
2024-02-18 10:45:45 +0100

    implemented tests for isPasswordActual func

--- src/isPasswordActual.test.js
+++ src/isPasswordActual.test.js
@@ -14,15 +14,31 @@ describe(`Function 'isPasswordActual':`, () => {
   });
 
   it(`should return a string`, () => {
+    const str = isPasswordActual(today.year, today.month, today.date);
 
+    expect(typeof str).toBe('string');
   });
 
-  it(`should ask to change the password if was changed a year ago`, () => {
-    const lastYear = isPasswordActual(today.year - 1, today.month, today.date);
+  // eslint-disable-next-line max-len
+  it(`should ask to change the password if was changed more than 60 days ago`, () => {
+    const lastYear = isPasswordActual(today.year, today.month - 2, today.date);
 
     expect(lastYear)
       .toBe('Immediately change the password!');
   });
 
-  // write more tests here
+  // eslint-disable-next-line max-len
+  it('should recommend to change the password if was changed a month ago', () => {
+    const lastMonth = isPasswordActual(today.year, today.month - 1, today.date);
+
+    expect(lastMonth).toBe('You should change your password.');
+  });
+
+  // eslint-disable-next-line max-len
+  it('should tell that password is actual if 30 days or less have changed', () => {
+    const inThisMonth
+      = isPasswordActual(today.year, today.month, today.date - 30);
+
+    expect(inThisMonth).toBe('Password is actual.');
+  });
 });
