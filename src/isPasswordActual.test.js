76ab9db6f1608de476e7ba3ebefc77fbbcec005d
Demian Zolotukhin <demianzolotukhin@gmail.com>
2024-10-23 14:40:59 +0300

    solution isPasswordActual

--- src/isPasswordActual.test.js
+++ src/isPasswordActual.test.js
@@ -2,6 +2,7 @@
 
 describe(`Function 'isPasswordActual':`, () => {
   const isPasswordActual = require('./isPasswordActual');
+
   const date = new Date(Date.now());
   const today = {
     year: date.getUTCFullYear(),
@@ -24,5 +25,35 @@ describe(`Function 'isPasswordActual':`, () => {
       .toBe('Immediately change the password!');
   });
 
-  // write more tests here
+  it(`should return 'Password is actual' 
+  message if func has no argument`, () => {
+    const lastYear = isPasswordActual();
+
+    expect(lastYear)
+      .toBe('Password is actual.');
+  });
+
+  it(`should return 'You should change your password.' 
+  if last password update < 60 days ago`, () => {
+    const lastYear = isPasswordActual(2024, 8, 25);
+
+    expect(lastYear)
+      .toBe('You should change your password.');
+  });
+
+  it(`should return 'Password is actual' 
+  if one of arguments is missing`, () => {
+    const lastYear = isPasswordActual(2024, 8);
+
+    expect(lastYear)
+      .toBe('Password is actual.');
+  });
+
+  it(`should return 'Password is actual' 
+  if string used as argument`, () => {
+    const lastYear = isPasswordActual('20g4', '8', '6');
+
+    expect(lastYear)
+      .toBe('Password is actual.');
+  });
 });
