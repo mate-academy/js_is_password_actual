a4096f212a93a74cb33bb5f51afc9811d2d6fc89
Yarolav <yaroslav@talentpath.co>
2024-11-20 16:07:27 +0200

    Solution

--- src/isPasswordActual.test.js
+++ src/isPasswordActual.test.js
@@ -1,7 +1,7 @@
-'use strict';
+"use strict";
 
 describe(`Function 'isPasswordActual':`, () => {
-  const isPasswordActual = require('./isPasswordActual');
+  const isPasswordActual = require("./isPasswordActual");
   const date = new Date(Date.now());
   const today = {
     year: date.getUTCFullYear(),
@@ -14,15 +14,31 @@ describe(`Function 'isPasswordActual':`, () => {
   });
 
   it(`should return a string`, () => {
+    const { date, month, year } = today;
+    const result = isPasswordActual(year, month, date);
 
+    expect(typeof result).toBe("string"); // Костиль but who knows
   });
 
   it(`should ask to change the password if was changed a year ago`, () => {
     const lastYear = isPasswordActual(today.year - 1, today.month, today.date);
 
-    expect(lastYear)
-      .toBe('Immediately change the password!');
+    expect(lastYear).toBe("Immediately change the password!");
   });
 
-  // write more tests here
+  it(`should ask to change the password if was changed a year ago`, () => {
+    const lastMonth = isPasswordActual(today.year, today.month - 1, today.date);
+
+    expect(lastMonth).toBe("You should change your password.");
+  });
+
+  it(`should not ask to change the password`, () => {
+    const upToDate = isPasswordActual(today.year, today.month, today.date);
+
+    expect(upToDate).toBe("Password is actual.");
+  });
+
+  it(`Should return 'password is actual if there is no data as a params'`, () => {
+    expect(isPasswordActual()).toBe("Password is actual.");
+  });
 });
