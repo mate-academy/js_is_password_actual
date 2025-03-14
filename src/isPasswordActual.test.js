5524adbaf1c4bf8af3de5634a5e454833c3cc689
Maria <m.gosp1704@gmail.com>
2025-03-14 13:02:28 +0200

    fix

--- src/isPasswordActual.test.js
+++ src/isPasswordActual.test.js
@@ -35,4 +35,18 @@ describe(`Function 'isPasswordActual':`, () => {
     expect(tenDaysAgo)
       .toBe('Password is actual.');
   });
+
+  it('should ask to change the password if it was changed exactly 30 days ago', () => {
+    const thirtyDaysAgo = isPasswordActual(today.year, today.month, today.date - 30);
+    expect(thirtyDaysAgo).toBe('Password is actual.');
+  });
+  it('should ask to change the password if it was changed exactly 60 days ago', () => {
+    const thirtyDaysAgo = isPasswordActual(today.year, today.month, today.date - 60);
+    expect(thirtyDaysAgo).toBe('You should change your password.');
+  });
+
+  it('should correctly handle January (month 0)', () => {
+    const firstJan = isPasswordActual(today.year, 0, today.date);
+    expect(typeof firstJan).toBe('string');
+  });
 });
