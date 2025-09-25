b2a7c1045d44c3825ea4438dffa56ac50a3d0e6e
Robert <robert.szalapak@gmail.com>
2025-09-25 08:25:43 +0200

    changing this values on normal values

--- src/isPasswordActual.test.js
+++ src/isPasswordActual.test.js
@@ -2,32 +2,18 @@
 
 describe(`Function 'isPasswordActual':`, () => {
   const isPasswordActual = require('./isPasswordActual');
-  const date = new Date(Date.now());
-  const today = {
-    year: date.getUTCFullYear(),
-    month: date.getMonth() + 1,
-    date: date.getDate(),
-  };
 
   it(`should be declared`, () => {
     expect(isPasswordActual).toBeInstanceOf(Function);
   });
 
   it(`should return a string`, () => {
-    expect(typeof isPasswordActual()).toBe('string');
-  });
-
-  it(`should ask to change the password immediately
-     if was changed a year ago`, () => {
-    const lastYear = isPasswordActual(today.year - 1, today.month, today.date);
-
-    expect(lastYear)
-      .toBe('Immediately change the password!');
+    expect(typeof isPasswordActual(2015, 5, 10)).toBe('string');
   });
 
   it(`should ask to change the password if 'days' >= 60`, () => {
     const lastYear
-     = isPasswordActual(today.year, today.month - 1, today.date - 30);
+     = isPasswordActual(2025, 6, 29);
 
     expect(lastYear)
       .toBe('Immediately change the password!');
@@ -35,14 +21,14 @@ describe(`Function 'isPasswordActual':`, () => {
 
   it(`should ask to change the password if 30 >= 'days' < 60`, () => {
     const lastYear
-     = isPasswordActual(today.year, today.month - 1, today.date - 29);
+     = isPasswordActual(2025, 8, 15);
 
     expect(lastYear)
       .toBe('You should change your password.');
   });
 
   it(`should inform your password is actual if 'days' <= 30`, () => {
-    const lastYear = isPasswordActual(today.year, today.month, today.date - 30);
+    const lastYear = isPasswordActual(2025, 8, 26);
 
     expect(lastYear)
       .toBe('Password is actual.');
