2f9e3c6676010df383de592b4011388cbf121b4c
Robert <robert.szalapak@gmail.com>
2025-09-25 08:04:05 +0200

    is_password_actual

--- src/isPasswordActual.test.js
+++ src/isPasswordActual.test.js
@@ -14,15 +14,37 @@ describe(`Function 'isPasswordActual':`, () => {
   });
 
   it(`should return a string`, () => {
-
+    expect(typeof isPasswordActual()).toBe('string');
   });
 
-  it(`should ask to change the password if was changed a year ago`, () => {
+  it(`should ask to change the password immediately
+     if was changed a year ago`, () => {
     const lastYear = isPasswordActual(today.year - 1, today.month, today.date);
 
     expect(lastYear)
       .toBe('Immediately change the password!');
   });
 
-  // write more tests here
+  it(`should ask to change the password if 'days' >= 60`, () => {
+    const lastYear
+     = isPasswordActual(today.year, today.month - 1, today.date - 30);
+
+    expect(lastYear)
+      .toBe('Immediately change the password!');
+  });
+
+  it(`should ask to change the password if 30 >= 'days' < 60`, () => {
+    const lastYear
+     = isPasswordActual(today.year, today.month - 1, today.date - 29);
+
+    expect(lastYear)
+      .toBe('You should change your password.');
+  });
+
+  it(`should inform your password is actual if 'days' <= 30`, () => {
+    const lastYear = isPasswordActual(today.year, today.month, today.date - 30);
+
+    expect(lastYear)
+      .toBe('Password is actual.');
+  });
 });
