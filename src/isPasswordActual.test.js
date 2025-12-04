3eb1f6c700d3054b9747322182e94c8377037c12
Albina <yakunina.albina@gmail.com>
2025-12-04 16:43:59 +0100

    fix tests

--- src/isPasswordActual.test.js
+++ src/isPasswordActual.test.js
@@ -3,7 +3,7 @@
 describe(`Function 'isPasswordActual':`, () => {
   const isPasswordActual = require('./isPasswordActual');
 
-  const MOCKED_NOW = new Date(2024, 2, 1).getTime();
+  const MOCKED_NOW = new Date(2021, 5, 10).getTime();
   let realDateNow;
 
   beforeAll(() => {
@@ -20,20 +20,20 @@ describe(`Function 'isPasswordActual':`, () => {
   });
 
   it(`should warn user to change password after 1 month`, () => {
-    const result = isPasswordActual(2021, 5, 15);
+    const result = isPasswordActual(2021, 5, 1);
 
     expect(result).toBe('You should change your password.');
   });
 
   it(`should ask to change the password if was changed a year ago`, () => {
-    const lastYear = isPasswordActual(2020, 5, 10);
+    const lastYear = isPasswordActual(2020, 5, 9);
 
     expect(lastYear)
       .toBe('Immediately change the password!');
   });
 
   it(`should confirm password is still actual`, () => {
-    const lastMonth = isPasswordActual(2021, 6, 10);
+    const lastMonth = isPasswordActual(2021, 6, 1);
 
     expect(lastMonth).toBe('Password is actual.');
   });
