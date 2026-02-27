f152c1e8d4aa1c8016cb269309a22f447835dc02
Dmytro Nikulin <dmytronikulin@Dmytros-MacBook-Air.local>
2026-02-27 18:53:18 +0200

    Add solution

--- src/isPasswordActual.test.js
+++ src/isPasswordActual.test.js
@@ -14,10 +14,12 @@ describe(`Function 'isPasswordActual':`, () => {
   });
 
   it(`should return a string`, () => {
-
+    expect(isPasswordActual(today.year, today.month, today.date))
+      .toBe('Password is actual.');
   });
 
-  it(`should ask to change the password if was changed a year ago`, () => {
+  it(`should ask to change the password immediately
+    if was changed a year ago`, () => {
     const lastYear = isPasswordActual(today.year - 1, today.month, today.date);
 
     expect(lastYear)
@@ -25,4 +27,49 @@ describe(`Function 'isPasswordActual':`, () => {
   });
 
   // write more tests here
+  it(`should tell the password is actual
+    if it was changed 30 days ago`, () => {
+    const lastYear = isPasswordActual(today.year, today.month, today.date - 30);
+
+    expect(lastYear)
+      .toBe('Password is actual.');
+  });
+
+  it(`should tell the password is actual
+    if it was changed less than 30 days ago`, () => {
+    const lastYear = isPasswordActual(today.year, today.month, today.date - 10);
+
+    expect(lastYear)
+      .toBe('Password is actual.');
+  });
+
+  it(`should tell to change the password
+    if it was changed 60 days ago`, () => {
+    const lastYear = isPasswordActual(
+      today.year, today.month - 1, today.date - 29
+    );
+
+    expect(lastYear)
+      .toBe('You should change your password.');
+  });
+
+  it(`should tell to change the password
+    if it was changed between 30 and 60 days ago`, () => {
+    const lastYear = isPasswordActual(
+      today.year, today.month - 1, today.date - 10
+    );
+
+    expect(lastYear)
+      .toBe('You should change your password.');
+  });
+
+  it(`should tell to immediately change the password
+    if it was changed more than 60 days ago`, () => {
+    const lastYear = isPasswordActual(
+      today.year, today.month - 3, today.date - 10
+    );
+
+    expect(lastYear)
+      .toBe('Immediately change the password!');
+  });
 });
