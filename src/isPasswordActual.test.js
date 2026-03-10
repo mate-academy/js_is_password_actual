01e8002fe76097469ee1d46ebf8b07dc1deb29dd
mykhalenych <mykhalenych@gmail.com>
2026-03-10 11:34:57 +0200

    feature/prettier added prettier and updated packages and node.js for gh actions

--- src/isPasswordActual.test.js
+++ src/isPasswordActual.test.js
@@ -13,15 +13,12 @@ describe(`Function 'isPasswordActual':`, () => {
     expect(isPasswordActual).toBeInstanceOf(Function);
   });
 
-  it(`should return a string`, () => {
-
-  });
+  it(`should return a string`, () => {});
 
   it(`should ask to change the password if was changed a year ago`, () => {
     const lastYear = isPasswordActual(today.year - 1, today.month, today.date);
 
-    expect(lastYear)
-      .toBe('Immediately change the password!');
+    expect(lastYear).toBe('Immediately change the password!');
   });
 
   // write more tests here
